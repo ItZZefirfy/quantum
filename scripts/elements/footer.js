8d29d57d4dee000425ecd909f5ce39b8ffee1de0
ItZZefirfy <its.demtin@gmail.com>
2025-05-12 21:46:19 +0400

    footer fix + authors list

--- scripts/elements/footer.js
+++ scripts/elements/footer.js
@@ -1,6 +1,20 @@
 function addFooter() {
     let footer = document.getElementsByTagName("footer")[0]
     let footerText =   `<div class="footer-layer">
+                            <a class="footer-layer" target="_blank" href="https://discord.gg/vX87VFVphg" style="user-select: none;">discord</a>
+                            <a class="footer-layer" target="_blank" href="" style="user-select: none;">Github</a>
+                            <a class="footer-layer" target="_blank" href="authors.html" style="user-select: none;">Авторы</a>
+                        </div>
+                        <div class="footer-layer">
+                            <version class="footer-item">version 3.0.0.0</version>
+                            <i class='footer-item bx bx-copyright'>Zefirfy</i>
+                        </div>`
+    
+    footer.innerHTML = footerText
+}
+
+/*
+`<div class="footer-layer">
                             <a class="footer-layer" href="" style="user-select: none;">discord</a>
                             <a class="footer-layer" href="" style="user-select: none;">telegram</a>
                             <a class="footer-layer" href="" style="user-select: none;">youtube</a>
@@ -13,6 +27,5 @@ function addFooter() {
                             <version class="footer-item">version 3.0.0.0</version>
                             <i class='footer-item bx bx-copyright'>Zefirfy</i>
                         </div>`
-    
-    footer.innerHTML = footerText
-}
+
+*/
